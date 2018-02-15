55b8839a04e3a7b633397de95b2b70a6ccb56f55
bo <602505401@qq.com>
2018-02-15 06:24:52 -0800

    fix

--- zmq_test/Service/server/server_config.py
+++ zmq_test/Service/server/server_config.py
@@ -2,10 +2,10 @@
 import zmq
 
 context = zmq.Context()
-server_zmq_addr = "tcp://119.28.111.197:9001"
+server_zmq_addr = "tcp://10.144.129.144:9001"
 server_to_client_subject = "server_to_client_subject"
 server_to_input_subject = "server_to_input_subject"
 
-server_zmq_addr_accept = "tcp://119.28.111.197:9002"
+server_zmq_addr_accept = "tcp://10.144.129.144:9002"
 client_to_server_subject = "client_to_server_subject"
 
